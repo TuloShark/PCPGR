7eba6ad6053c40608f87034811c1fa3935eb34c1
NoraaJO <aaronortizjimenez@gmail.com>
2024-04-12 13:50:20 -0600

    se agregaron los archivos

---
+++ areas.py
@@ -0,0 +1,12 @@
+class areas:
+    
+    maxDesarrolladores = 0
+    maxDiseno = 0
+    maxMarketing = 0
+    maxContabilidad = 0
+    
+    def __init__(self, maxAreaDis, maxAreaDevs, maxAreaMarke, maxAreaContabilidad):
+       self.maxDesarrolladores = maxAreaDevs
+       self.maxDiseno = maxAreaDis
+       self.maxMarketing = maxAreaMarke
+       self.maxContabilidad = maxAreaContabilidad
