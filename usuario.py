3b4a69faf7c4a3b7a8b1c31582a09962581884d1
NoraaJO <aaronortizjimenez@gmail.com>
2024-04-12 14:40:33 -0600

    Se agrego usuario y se programaron los metos de area

---
+++ usuario.py
@@ -0,0 +1,35 @@
+class usuario:
+    id = ""
+    nombre = ""
+    apellido = ""
+    cedula = ""
+    telefono = ""
+    oficina = ""
+    sede = ""
+    
+    def __init__(self, id, nombre, apellido, cedula, telefono, oficina, Sede):
+        self.id = id
+        self.nombre = nombre
+        self.apellido = apellido
+        self.cedula = cedula
+        self.telefono = telefono
+        self.oficina = oficina
+        self.sede = Sede
+    def toString(self):
+        return f'Nombre: {self.nombre} Apellido: {self.apellido}, Cedula: {self.cedula}, 
+        Telefono: {self.telefono}, Oficina: {self.oficina}, Sede: {self.sede}'
+    #Seccion GETTER
+    def getId(self):
+        return self.id
+    def getNombre(self):
+        return self.nombre
+    def getApellido(self):
+        return self.apellido
+    def getCedula(self):
+        return self.cedula
+    def getTelefono(self):
+        return self.telefono
+    def getOficina(self):
+        return self.oficina
+    def getSede(self):
+        return self.sede
